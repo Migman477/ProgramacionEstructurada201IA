2251c0f9653ddd0d96ad38e762cbc0b9ea0a71e3
Miguel <miguelgermancruzmaillard2@gmail.com>
2026-03-10 07:27:00 -0600

    comentarios

---
+++ src/ux.com.curso.progrmacion/unidad1/faker/comentarios.py
@@ -0,0 +1,9 @@
+#tipos de coentarios en Python
+
+"""
+comentario multilinea
+"""
+
+print("Tipos de comentarios en Python")#comentario de una sola linea
+
+#comentar con ua linea el flujo del codigo y con multilinea las funciones
