2348aa8b09796c4acae2f6eda3c7009fdd94a314
Miguel <miguelgermancruzmaillard2@gmail.com>
2026-03-10 08:40:59 -0600

    calificaciones

--- src/ux.com.curso.progrmacion/unidad1/Ejercicios_induccion/calificaciones.py
+++ src/ux.com.curso.progrmacion/unidad1/Ejercicios_induccion/calificaciones.py
@@ -9,7 +9,10 @@ def asignar_calificaciones ():
     elif calificacion >=80 and calificacion <90:
         print("Tu calificacion es B")
     
-    elif calificacion >= 69 and calificacion <80:
+    elif calificacion >=70 and calificacion <800:
+        print("Tu calificacion es C")
+    
+    elif calificacion == 69:
         print("Tu calificacion es D")
     
     else:
