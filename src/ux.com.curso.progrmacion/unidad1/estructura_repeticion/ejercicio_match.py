f4bd6d94669334528a1dae666f707070d6ea6e82
Miguel <miguelgermancruzmaillard2@gmail.com>
2026-03-13 08:41:47 -0600

    ejercicios_match

---
+++ src/ux.com.curso.progrmacion/unidad1/estructura_repeticion/ejercicio_match.py
@@ -0,0 +1,30 @@
+#Implementacion de Match en Python
+
+def demstracion():
+    print("--Ejemplo Match--")
+    opcion = input("Ingrese una opcion (1-3)")
+
+    match opcion:
+        case"1":
+            print ("Opcion 1 seleccionada")
+            nombre = input("Ingrese su nombre: ")
+            print(f"Hola {nombre}")
+
+        case"2":
+            print ("Opcion 2 seleccionada")
+            matricula = input("Ingrese su matricula: ")
+            print(f"Su matricula es {matricula}")
+        
+        case"3":
+            print ("Opcion 3 seleccionada")
+            semestre = input("Ingrese su semestre: ")
+            print(f"Usted esta en el semestre {semestre}")
+        
+        case _:
+            print("Opcion no valida")
+
+def main():
+    demstracion()
+
+if __name__ == "__main__":
+    main()
