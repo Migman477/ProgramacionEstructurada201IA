733aae94480a4d0bf7c5ba7394504184b9f5ecd7
Miguel <miguelgermancruzmaillard2@gmail.com>
2026-03-17 07:35:41 -0600

    ejercicios_aritmetica2

---
+++ src/ux.com.curso.progrmacion/unidad1/Ejercicios_aritmetica/Ejercicio_aritmetica_2.py
@@ -0,0 +1,25 @@
+
+import math as m
+
+#demostracion del uso de funcionws de math
+
+def mostran_funciones_math():
+    #Crear una variable
+    numero = 20
+
+    #Calcular la raiz cuadrada
+    sen_x = m.sin(numero)
+    cos_x = m.cos(numero)
+
+    print(f"El seno de {numero} es {sen_x}")
+    print(f"El seno de {numero} es {sen_x}")
+
+    resultado = cos_x**2 + sen_x**2
+
+    print(f"El resultado de la suma de los cuadrados del sen y cos de ex es {resultado}")
+
+def main ():
+    mostran_funciones_math()
+
+if __name__ == "__main__":
+    main()
