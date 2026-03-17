04ddaac3546d1781ad129ee516a237b0f2a1a8c2
Miguel <miguelgermancruzmaillard2@gmail.com>
2026-03-17 07:22:16 -0600

    ejercicios_aritmetica

---
+++ src/ux.com.curso.progrmacion/unidad1/Ejercicios_aritmetica/Ejercicio_aritmetica.py
@@ -0,0 +1,32 @@
+#ejemplo de uso de operadores aritmeticos en python
+def operaciones_aritmeticas ():
+    # Suma
+    suma = 5 + 3
+
+    #Resta
+    resta = 10 - 4
+
+    #Multiplicacion
+    multiplicacion = 6 * 7
+
+    Division_entera = 10 // 3
+
+    #Modulo
+
+    modulo = 10 % 3
+
+    #Potenciacion
+
+    potenciacion = 2 ** 3
+
+    print(f"La suma es: {suma}")
+    print(f"La resta es: {resta} ")
+    print(f"La multiplicacion es: {multiplicacion}")
+    print(f"La Division entera es: {Division_entera}")
+    print(f"El modulo es: {modulo}")
+
+def main ():
+    operaciones_aritmeticas()
+
+if __name__ == "__main__":
+    main()
