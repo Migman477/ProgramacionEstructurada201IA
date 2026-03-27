5291797c5f96c288eae515e1ee536d12f9967f4a
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-03-27 08:12:20 -0600

    Mermaid_01

--- src/ux.com.curso.progrmacion/unidad1/Examen_unida-_1/Entrenamiento_lotes.py
+++ src/ux.com.curso.progrmacion/unidad1/Examen_unida-_1/Entrenamiento_lotes.py
@@ -1,4 +1,4 @@
-#Simulación de entrenamiento por lotes con límite de memoria RAM
+#Simulación de entrenamiento por lotes con límite de memoria RAM .31336..
 # El programa solicita al usuario que ingrese el peso de cada lote en MB y acumula el peso total. Si el peso total supera los 2500 MB, se muestra un mensaje de "Out of memory" y el programa termina.
 def obtener_lotes():
     V_RAM = 0
---
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/ejercicio01.py
@@ -0,0 +1,5 @@
+"""
+Diseña un algoritmo que calcule la suma de todos los numeros enteros
+del 1 al 100 que son divisibles por 3 y ademas impares
+"""
+
