255d57060f9d14ce13af3647fbf6cd593357f3ee
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-03-27 08:32:33 -0600

    Mermaid_01

--- src/ux.com.curso.progrmacion/unidad2/Mermaid/ejercicio01.py
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/ejercicio01.py
@@ -2,4 +2,22 @@
 Diseña un algoritmo que calcule la suma de todos los numeros enteros
 del 1 al 100 que son divisibles por 3 y ademas impares
 """
+def es_divisible_por_3_y_impar():
+    suma = 0
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 2 != 0:
+            suma += i
+        i += 1
+        
+    return suma
 
+
+
+def main():
+    
+    print(f"La suma de los numeros enteros del 1 al 100 que son divisibles por 3 y ademas impares es: {es_divisible_por_3_y_impar()}")
+    
+
+if __name__ == "__main__":
+    main()
