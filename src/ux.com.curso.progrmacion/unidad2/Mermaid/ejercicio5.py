3ab9301958b809dd7fc25c11fcf6778c8fa507ad
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-07 07:33:10 -0600

    Suma acumulativa-version2

--- src/ux.com.curso.progrmacion/unidad2/Mermaid/ejercicio5.py
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/ejercicio5.py
@@ -1,24 +1,28 @@
 """
 Ejercicio 4: Suma de números hasta superar 500
-
-Escribe un programa que solicite al usuario ingresar números enteros y los sume.
- El programa debe continuar solicitando números hasta que la suma total supere los 500.
+Escribe un programa que solicite al usuario ingresar números enteros y los sume. El programa debe continuar solicitando números hasta que la suma total supere los 500.
 Una vez que se alcance o supere ese límite, el programa debe mostrar la suma total de los números ingresados.
 """
 suma = 0
 
 def calcular_suma():
     global suma
-    while suma <= 500:
+
+    while True:
+
         numero = float(input("Ingrese un número: "))
-        suma += numero
+        
+        if numero >10 and numero <= 50:
+            suma += numero
+        else:
+            break
     return suma
+        
 
 def main():
-   calcular_suma()
+    calcular_suma()
 
 print(f"La suma de los números ingresados es: {calcular_suma()}")
 
 if __name__ == "__main__":
     main()
-    
