dc3e967141ba034e7cd0dd3d44133329f9f01f79
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-07 23:26:26 -0600

    feat: inicializar proyecto y saludo del asistente

---
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/asistente_logico.py
@@ -0,0 +1,22 @@
+"""Asistente lógico en Python
+Este programa simula un asistente lógico que responde a diferentes solicitudes del usuario.
+El asistente puede saludar, proporcionar información sobre el clima, la hora y despedirse.
+"""
+
+import datetime
+
+globalnombre_asistente = "Lin"
+
+def obtener_solicitud():
+  
+
+    print(f"Hola, soy {globalnombre_asistente}, tu asistente lógico")
+
+    solicitud = input("¿En qué puedo ayudarte hoy? : ").lower()
+ 
+    return solicitud
+
+
+
+
+
