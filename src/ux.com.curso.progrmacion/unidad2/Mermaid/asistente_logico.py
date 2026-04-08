ad3c46067628b0b01058d0c8944d28e0c30532f1
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-07 23:41:05 -0600

    feat: implementar detección de intención

--- src/ux.com.curso.progrmacion/unidad2/Mermaid/asistente_logico.py
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/asistente_logico.py
@@ -3,7 +3,7 @@ Este programa simula un asistente lógico que responde a diferentes solicitudes 
 El asistente puede saludar, proporcionar información sobre el clima, la hora y despedirse.
 """
 
-
+import datetime
 
 globalnombre_asistente = "Lin"
 
@@ -15,6 +15,24 @@ def obtener_solicitud():
     solicitud = input("¿En qué puedo ayudarte hoy? : ").lower()
  
     return solicitud
+
+def detectar_intencion(solicitud):
+
+    if "hola" in solicitud or "buenos dias" in solicitud or "buenas tardes" in solicitud or "buenas noches" in solicitud:
+        return "¡Hola! Soy tu asistente. Es un gusto saludarte"
+    
+    elif "clima"in solicitud or "temperatura" in solicitud:
+        return "Consultando el servicio meteorológico... Hoy en Xalapa tendremos un día nublado."
+    
+    elif "hora" in solicitud or "tiempo" in solicitud:
+        return "La hora actual del sistema es: " + datetime.datetime.now().strftime("%H:%M:%S")
+    
+    else:
+        return "Lo siento, todavía no entiendo ese comando. ¿Podrías intentar con otra palabra?"
+    
+    
+
+
 
 
 
