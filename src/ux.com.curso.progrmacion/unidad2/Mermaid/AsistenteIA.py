a9298d40b7a4ff0def807912c368732657b9e7fc
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-08 21:28:55 -0600

    Crear una funcion para capturar la instruccion y el nivel de confianza

--- src/ux.com.curso.progrmacion/unidad2/Mermaid/AsistenteIA.py
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/AsistenteIA.py
@@ -1,5 +1,4 @@
 
 
-global UMBRAL_ALTO = 80.0 #(Confianza suficiente para actuar).
-
-global UMBRAL_MINIMO = 40.0 #(Confianza mínima para intentar sugerir algo).
+UMBRAL_ALTO = 80.0 #(Confianza suficiente para actuar).
+UMBRAL_MINIMO = 40.0 #(Confianza mínima para intentar sugerir algo).
