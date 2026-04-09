00c01afa5a6403d4414c2dbf70f82de7eda06bdc
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-08 21:29:45 -0600

    Crear una funcion para capturar la instruccion y el nivel de confianzaa

--- src/ux.com.curso.progrmacion/unidad2/Mermaid/AsistenteIA.py
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/AsistenteIA.py
@@ -2,3 +2,8 @@
 
 UMBRAL_ALTO = 80.0 #(Confianza suficiente para actuar).
 UMBRAL_MINIMO = 40.0 #(Confianza mínima para intentar sugerir algo).
+
+def captura_datos():
+    instruccion = input("Ingrese su instrucción: ")
+    nivel_confianza = float(input("Ingrese el nivel de confianza (0-100): "))
+    return instruccion, nivel_confianza
