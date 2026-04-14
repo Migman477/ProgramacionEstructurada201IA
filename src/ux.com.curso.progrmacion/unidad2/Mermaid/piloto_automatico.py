d6e980f2f49951920d1424a1af7dcec71e128cbd
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-04-13 19:03:03 -0600

    configurar variables del piloto automatico

---
+++ src/ux.com.curso.progrmacion/unidad2/Mermaid/piloto_automatico.py
@@ -0,0 +1,4 @@
+#Piloto automático para un vehículo
+distancia_objeto = float(input("Ingrese la distancia al objeto mas cercano (en metros): "))
+color_semaforo = input("Ingrese el color del semaforo (rojo, amarillo, verde): ").lower()
+peaton_cruzando = input("¿Hay un peatón cruzando? (si/no): ").lower()
