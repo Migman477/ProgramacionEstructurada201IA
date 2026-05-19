3fdc4b07b53317a3987d66959bc601ccf9e68779
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-05-19 07:18:40 -0600

    Estructura unidad 4

---
+++ src/ux.com.curso.progrmacion/unidad4/practica02/geston_inventario.py
@@ -0,0 +1,7 @@
+import re
+def listar_librerias(codigo):
+    # Utilizamos una expresión regular para encontrar las líneas que importan librerías
+    librerias = re.findall(r'^\s*(import|from)\s+(\w+)', codigo, re.MULTILINE)
+    # Extraemos solo los nombres de las librerías
+    nombres_librerias = [lib[1] for lib in librerias]
+    return "Funciones Externas (Bibliotecas):\n" + "\n".join(nombres_librerias)
