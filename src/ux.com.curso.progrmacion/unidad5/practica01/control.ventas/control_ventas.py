b7242718e57049a43b82953e7cf1c48e05f1c324
Migman477 <miguelgermancruzmaillard2@gmail.com>
2026-06-01 07:38:33 -0600

    UPDATE- Creacion de la funcion para obtener las vrntas del usuario y mostrar el producto con ventas mayores en la seman

--- src/ux.com.curso.progrmacion/unidad5/practica01/control.ventas/control_ventas.py
+++ src/ux.com.curso.progrmacion/unidad5/practica01/control.ventas/control_ventas.py
@@ -1,41 +1,104 @@
+
+
+# ==================FUNCIONES==================
+
 # Declaración de estructuras
 
 productos = ["Laptop", "Smartphone", "Tablet"]
 
 ventas = [[0] * 3 for _ in range(3)]
 
+#obtener ventas de cada producto por día pidiendolas al usuario
+
+
+def obtener_ventas():
+
+    for i in range(3):
+
+        print(f"--- Registro para {productos[i]} ---")
+
+        for j in range(3):
+
+            ventas[i][j] = int(input(f"Ventas del día {j+1}: "))
+
  
 
 # Lectura de datos
 
-for i in range(3):
+def lectura_datos():
 
-    print(f"--- Registro para {productos[i]} ---")
+    for i in range(3):
 
-    for j in range(3):
+        print(f"--- Registro para {productos[i]} ---")
 
-        ventas[i][j] = int(input(f"Ventas del día {j+1}: "))
+        for j in range(3):
+
+            ventas[i][j] = int(input(f"Ventas del día {j+1}: "))
+
+    return ventas
 
  
 
 # Escritura y Reporte
 
-print("\nRESUMEN DE VENTAS")
+def reporte_ventas(ventas, productos):
 
-total_general = 0
+    print("\nRESUMEN DE VENTAS")
+
+    total_general = 0
+
+
+
+    print("\nRESUMEN DE VENTAS")
+
+    total_general = 0
+
+    
+
+    for i in range(3):
+
+        suma_producto = sum(ventas[i])
+
+        total_general += suma_producto
+
+        print(f"{productos[i]}: {ventas[i]} | Total: {suma_producto}")
+
+    
+
+    print(f"\nEl total de ventas de la semana es: {total_general}")
+
+    print(f"El promedio de ventas es: {total_general / 9:.2f}")
+
+def producto_mas_vendido():
+
+    max_ventas = 0
+
+    producto_mas_vendido = ""
 
  
 
-for i in range(3):
+    for i in range(3):
 
-    suma_producto = sum(ventas[i])
+        suma_producto = sum(ventas[i])
 
-    total_general += suma_producto
+        if suma_producto > max_ventas:
 
-    print(f"{productos[i]}: {ventas[i]} | Total: {suma_producto}")
+            max_ventas = suma_producto
+
+            producto_mas_vendido = productos[i]
 
  
 
-print(f"\nEl total de ventas de la semana es: {total_general}")
+    print(f"\nEl producto más vendido es: {producto_mas_vendido} con {max_ventas} ventas.")
 
-print(f"El promedio de ventas es: {total_general / 9:.2f}")
+def main():
+    obtener_ventas()
+
+    reporte_ventas(ventas, productos)
+
+    producto_mas_vendido()
+ 
+
+if __name__ == "__main__":
+
+    main()
